549cb6c2adffed305e7b211770be9838541a8578
JoseTorres24 <125532998+JoseTorres24@users.noreply.github.com>
2024-08-24 09:54:57 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var textoBarra = document.getElementById('barra');
+
+function Imprimirtexto (){
+    var texto = textoBarra.value;
+    console.log(texto);
+}
+function borrarTexto(){
+    textoBarra.value = '';
+}
